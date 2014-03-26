cab4f180f53f4a108baf5d563944014d83504027
Jiangge Zhang <tonyseek@gmail.com>
2014-03-27 04:04:24 +0800

    build magic binding with cffi instead of cython.

---
+++ hamish/_magic.py
@@ -0,0 +1,104 @@
+import cffi
+
+__version__ = "0.0.1"
+
+# Flag constants for open and setflags
+MAGIC_NONE = NONE = 0
+MAGIC_DEBUG = DEBUG = 1
+MAGIC_SYMLINK = SYMLINK = 2
+MAGIC_COMPRESS = COMPRESS = 4
+MAGIC_DEVICES = DEVICES = 8
+MAGIC_MIME_TYPE = MIME_TYPE = 16
+MAGIC_CONTINUE = CONTINUE = 32
+MAGIC_CHECK = CHECK = 64
+MAGIC_PRESERVE_ATIME = PRESERVE_ATIME = 128
+MAGIC_RAW = RAW = 256
+MAGIC_ERROR = ERROR = 512
+MAGIC_MIME_ENCODING = MIME_ENCODING = 1024
+MAGIC_MIME = MIME = 1040
+MAGIC_APPLE = APPLE = 2048
+
+MAGIC_NO_CHECK_COMPRESS = NO_CHECK_COMPRESS = 4096
+MAGIC_NO_CHECK_TAR = NO_CHECK_TAR = 8192
+MAGIC_NO_CHECK_SOFT = NO_CHECK_SOFT = 16384
+MAGIC_NO_CHECK_APPTYPE = NO_CHECK_APPTYPE = 32768
+MAGIC_NO_CHECK_ELF = NO_CHECK_ELF = 65536
+MAGIC_NO_CHECK_TEXT = NO_CHECK_TEXT = 131072
+MAGIC_NO_CHECK_CDF = NO_CHECK_CDF = 262144
+MAGIC_NO_CHECK_TOKENS = NO_CHECK_TOKENS = 1048576
+MAGIC_NO_CHECK_ENCODING = NO_CHECK_ENCODING = 2097152
+
+MAGIC_NO_CHECK_BUILTIN = NO_CHECK_BUILTIN = 4173824
+
+MAGIC_VERSION = 517
+
+
+ffi = cffi.FFI()
+ffi.cdef("""
+    struct magic_set;
+
+    struct magic_set *magic_open(int);
+    void magic_close(struct magic_set *);
+
+    const char *magic_getpath(const char *, int);
+    const char *magic_file(struct magic_set *, const char *);
+    const char *magic_descriptor(struct magic_set *, int);
+    const char *magic_buffer(struct magic_set *, const void *, size_t);
+
+    const char *magic_error(struct magic_set *);
+    int magic_setflags(struct magic_set *, int);
+
+    int magic_version();
+    int magic_load(struct magic_set *, const char *);
+    int magic_compile(struct magic_set *, const char *);
+    int magic_check(struct magic_set *, const char *);
+    int magic_list(struct magic_set *, const char *);
+    int magic_errno(struct magic_set *);
+""")
+cmagic = ffi.verify(
+    '#include "magic.h"',
+    include_dirs=['vendor/include'],
+    library_dirs=['vendor/lib'],
+    libraries=["magic_embed", "z_embed"]
+)
+
+
+class Magic(object):
+    def __init__(self, magic_flag=None, magic_file=None):
+        self._c_magic_t = ffi.NULL
+        self.magic_flag = MAGIC_NONE if magic_flag is None else magic_flag
+        self.magic_file = ffi.NULL if magic_file is None else magic_file
+
+    def __enter__(self):
+        self.open(self.magic_flag)
+        self.load(self.magic_file)
+        return self
+
+    def __exit__(self, error_type, error_value, error_tb):
+        self.close()
+
+    def open(self, flags):
+        self._c_magic_t = cmagic.magic_open(flags)
+
+    def close(self):
+        cmagic.magic_close(self._c_magic_t)
+
+    def load(self, path):
+        cmagic.magic_load(self._c_magic_t, path)
+
+    def from_file(self, filename):
+        r = cmagic.magic_file(self._c_magic_t, filename)
+        if r:
+            return ffi.string(r)
+
+    def from_buffer(self, buf):
+        """
+        Returns a textual description of the contents of the argument passed
+        as a buffer or None if an error occurred and the MAGIC_ERROR flag
+        is set. A call to errno() will return the numeric error code.
+        """
+        c_buf = ffi.new('char []', buf)
+        c_buf_len = len(c_buf)
+        r = cmagic.magic_buffer(self._c_magic_t, c_buf, c_buf_len)
+        if r:
+            return ffi.string(r)
--- setup.py
+++ setup.py
@@ -1,12 +1,41 @@
-# -*- coding: utf-8 -*-
 import os
+import sys
 import tarfile
 from subprocess import Popen, PIPE
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, find_packages
+from setuptools.dist import Distribution
+from importlib import import_module
+
+
+class CFFIExtension(object):
+    def __init__(self, qualname):
+        self._qualname = qualname
+        self._ffi_cached = None
+        # install the cffi
+        Distribution({"setup_requires": ["cffi"]})
+
+    @property
+    def _ffi(self):
+        if not self._ffi_cached:
+            modname, ffiname = self._qualname.rsplit(":", 1)
+            mod = import_module(modname)
+            ffi = getattr(mod, ffiname)
+            self._ffi_cached = ffi.verifier.get_extension()
+        return self._ffi_cached
+
+    def __getattribute__(self, name):
+        if name in ("_ffi", "_ffi_cached", "_qualname"):
+            return object.__getattribute__(self, name)
+        return getattr(self._ffi, name)
+
+    def __setattr__(self, name, value):
+        if name in ("_ffi", "_ffi_cached", "_qualname"):
+            return object.__setattr__(self, name, value)
+        return setattr(self._ffi, name, value)
+
 
 # setuptools DWIM monkey-patch madness
-# http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
-import sys
+# http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204 # noqa
 if 'setuptools.extension' in sys.modules:
     m = sys.modules['setuptools.extension']
     m.Extension.__dict__ = m._Extension.__dict__
@@ -16,7 +45,8 @@ PYTHON3K = sys.version_info[0] > 2
 cwd = os.path.dirname(os.path.realpath(__file__))
 vendor_path = os.path.join(cwd, 'vendor')
 
-def excute(command_list, shell=False, wait=False):
+
+def execute(command_list, shell=False, wait=False):
     popen = Popen(command_list, stdout=PIPE, stderr=PIPE, shell=shell)
     if wait:
         popen.wait()
@@ -24,6 +54,7 @@ def excute(command_list, shell=False, wait=False):
     if popen.returncode != 0:
         print(stderrdata)
         sys.exit()
+
 
 def extract(file):
     t = tarfile.open(file, mode='r:gz')
@@ -41,21 +72,21 @@ libmagic_path = os.path.join(vendor_path, 'file-5.17')
 os.chdir(libmagic_path)
 
 # build libmagic
-excute(['patch -p0 < ../file-locale-5.17.patch'], shell=True, wait=True)
+execute(['patch -p0 < ../file-locale-5.17.patch'], shell=True, wait=True)
 
-excute(['./configure', '--prefix=%s' % vendor_path, '--disable-shared',
-         '--enable-static', '--with-pic'])
+execute(['./configure', '--prefix=%s' % vendor_path, '--disable-shared',
+        '--enable-static', '--with-pic'])
 
-excute(['make', '-C', 'src', 'install'])
+execute(['make', '-C', 'src', 'install'])
 
-excute(['make', '-C', 'magic', 'install'])
+execute(['make', '-C', 'magic', 'install'])
 
 # build libz
 libz_path = os.path.join(vendor_path, 'zlib-1.2.8')
 os.chdir(libz_path)
-excute(['./configure', '--prefix=%s' % vendor_path, '--static'])
+execute(['./configure', '--prefix=%s' % vendor_path, '--static'])
 
-excute(['make', 'install'])
+execute(['make', 'install'])
 
 # prepare embed lib
 os.rename(os.path.join(vendor_path, 'lib', 'libmagic.a'),
@@ -72,13 +103,8 @@ setup(
     description="A wrapper for libmagic with static build.",
     author="XTao",
     author_email='xutao881001@gmail.com',
-    setup_requires=['setuptools_cython'],
-    install_requires=['cython>=0.20'],
-    ext_modules=[Extension("_magic",
-                           ["src/magic.pyx"],
-                           include_dirs=['vendor/include'],
-                           library_dirs=['vendor/lib'],
-                           libraries=["magic_embed", "z_embed"])],
+    install_requires=['cffi'],
+    ext_modules=[CFFIExtension("hamish._magic:ffi")],
     py_modules=['hamish'],
     packages=find_packages(),
     package_data={'': ['misc/magic.mgc']},
